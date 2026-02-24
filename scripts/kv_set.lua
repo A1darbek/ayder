2479eb41f47f49ba391523d52a1f1774a33ee650
Aidarbek <amanova1978@gmail.com>
2026-02-25 00:53:14 +0500

    Delete scripts/kv_set.lua

--- scripts/kv_set.lua
+++
@@ -1,9 +0,0 @@
-wrk.method = "POST"
-local ns = "global"
-function request()
-  local id = math.random(1, 100000000)
-  local key = string.format("k%08d", id)
-  local body = string.format('{"value":"v%08d"}', id)
-  local path = string.format("/v1/kv/%s/%s", ns, key)
-  return wrk.format("POST", path, {["Content-Type"]="application/json"}, body)
-end
