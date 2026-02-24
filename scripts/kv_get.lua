4a8195d43e6eaaceb2e6813b1e056d499312b71e
Aidarbek <amanova1978@gmail.com>
2026-02-25 00:53:21 +0500

    Delete scripts/kv_get.lua

--- scripts/kv_get.lua
+++
@@ -1,8 +0,0 @@
-wrk.method = "GET"
-local ns = "global"
-function request()
-  local id = math.random(1, 100000000)
-  local key = string.format("k%08d", id)
-  local path = string.format("/v1/kv/%s/%s", ns, key)
-  return wrk.format("GET", path)
-end
